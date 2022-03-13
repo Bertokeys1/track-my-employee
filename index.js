b7b58d96dc7bc92ef7a69cecf268f924928dd0ea
Bertokeys1 <abarrientos93.ab@gmail.com>
2022-03-13 11:08:21 -0700

    updated db files. getting help from tutor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const util = require('util');
 const table = require('console.table');
 const db = require('./db/connection');
 
-// syncs query with utils
+// syncs query with utils to print strings and data
 db.query = util.promisify(db.query);
 
 const firstQuestion =[

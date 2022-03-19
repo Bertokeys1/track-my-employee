937eda916982c982c49b18867fa0bd373b64226c
Bertokeys1 <abarrientos93.ab@gmail.com>
2022-03-18 21:15:19 -0700

    updated index with tutor

--- db/query.js
+++ db/query.js
@@ -4,4 +4,7 @@ class Directory {
     constructor(connection){
         this.connection = connection
     }
+//create the query that is needed. example findAllDepartments make usre to use promise from mysql2
 }
+
+module.exports =  new Directory(connection)
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const inquirer = require('inquirer');
-const table = require('console.table');
-const db = require('./db/connection');
+require('console.table');
+// const db = require('./db/connection');
 
+function start(){
+prompts()
+}
 
-const firstQuestion =[
+function prompts(){
+    inquirer.prompt([
+        {
+            type: 'list', 
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+        },
+    ]).then((response) => {
+        //use a switch statment
+        if (response.choice === "View all departments"){
+// create a fucntion that runs here
 
-]
+        }
+    })
+}
+
+start();
